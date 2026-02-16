b4296441e890d963e70f51c40b8524e9f2529dcc
saraargh <saraargh1@gmail.com>
2026-02-16 13:47:15 +0000

    Update selfroles.py

--- selfroles.py
+++ selfroles.py
@@ -227,8 +227,6 @@ def role_manageable(role: discord.Role, me: discord.Member) -> bool:
         return False
     if role.managed:
         return False
-    if role.permissions.administrator:
-        return False
     if role >= me.top_role:
         return False
     return True
